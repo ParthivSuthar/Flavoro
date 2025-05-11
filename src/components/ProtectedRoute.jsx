af819e3cb81cdbe10d3fe9842b22d683b1871a0d
Parthiv Suthar <myworkflow7070@gmail.com>
2025-05-11 20:48:55 +0530

    add react spinner package for loading and add protected routes for smooth navigation

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ element }) {
+  const cartItems = useSelector((state) => state.cart.cart);
+
+  return cartItems.length > 0 ? element : <Navigate to="/" />;
+}
+
+export default ProtectedRoute;
