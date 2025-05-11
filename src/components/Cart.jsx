0c3858de7bf0f9da460c5266ba9a81e15305a0c5
Parthiv Suthar <myworkflow7070@gmail.com>
2025-05-11 09:19:16 +0530

    implement cart functionality like calculate total price & item, increase and decrease item quantity

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,6 +7,14 @@ import { useSelector } from "react-redux";
 function Cart() {
   const cartItems = useSelector((state) => state.cart.cart);
   console.log(cartItems);
+  const totalQty = cartItems.reduce(
+    (totalQty, item) => totalQty + item.qty,
+    0
+  );
+  const totalPrice = cartItems.reduce(
+    (total, item) => total + item.price * item.qty,
+    0
+  );
   const [activeCart, setActiveCart] = useState(true);
   return (
     <>
@@ -23,20 +31,26 @@ function Cart() {
           />
         </div>
 
-        {cartItems.length > 0 ? cartItems.map((food) => (
-          <ItemCard
-            key={food.id}
-            id={food.id}
-            name={food.name}
-            price={food.price}
-            img={food.img}
-            qty={food.qty}
-          />
-        )) : <h2 className="text-center text-xl font-bold text-gray-800">Your cart is empty</h2>} 
+        {cartItems.length > 0 ? (
+          cartItems.map((food) => (
+            <ItemCard
+              key={food.id}
+              id={food.id}
+              name={food.name}
+              price={food.price}
+              img={food.img}
+              qty={food.qty}
+            />
+          ))
+        ) : (
+          <h2 className="text-center text-xl font-bold text-gray-800">
+            Your cart is empty
+          </h2>
+        )}
 
         <div className="absolute bottom-0">
-          <h3 className="font-semibold text-gray-800">Items: </h3>
-          <h3 className="font-semibold text-gray-800">Total Amount: </h3>
+          <h3 className="font-semibold text-gray-800">Items: {totalQty}</h3>
+          <h3 className="font-semibold text-gray-800">Total Amount: {totalPrice}</h3>
           <hr className="w-[90vw] lg:w-[18vw] my-2" />
           <button className="bg-green-500 font-bold px-3 text-white py-2 rounded-lg w-[90vw] lg:w-[18vw] mb-5">
             Checkout
@@ -45,7 +59,9 @@ function Cart() {
       </div>
       <FaShoppingCart
         onClick={() => setActiveCart(!activeCart)}
-        className="rounded-full bg-white shadow-md text-5xl p-3 fixed bottom-4 right-4"
+        className={`rounded-full bg-white shadow-md text-5xl p-3 fixed bottom-4 right-4 ${
+          totalQty > 0 && "animate-bounce delay-500 transition-all"
+        }`}
       />
     </>
   );
