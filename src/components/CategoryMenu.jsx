3dc729c399ea91413c0344177d9b7690a7fee747
Parthiv Suthar <myworkflow7070@gmail.com>
2025-05-11 10:10:23 +0530

    add category dynamically from api data and create category slice and add in store

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import FoodData from "./../data/FoodData";
+import { useDispatch, useSelector } from "react-redux";
+import { setCategory } from "../redux/slices/CategorySlice";
 
 function CategoryMenu() {
+  const dispatch = useDispatch()
+  const selectedCategory = useSelector((state) => state.category.category)
+  const [categories, setCategories] = useState([]);
+
+  const listUniqueCategories = () => {
+    const uniqueCategories = [
+      ...new Set(FoodData.map((food) => food.category)),
+    ];
+    setCategories(uniqueCategories);
+    console.log(uniqueCategories);
+  };
+
+  useEffect(() => {
+    listUniqueCategories();
+  }, []);
   return (
-    <div className='ml-6'>
-        <h3 className='text-xl font-semibold'>Find the best food</h3>
-        <div className='my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-x-hidden'>
-            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>All</button>
-            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Lunch</button>
-            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Breakfast</button>
-            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Dinner</button>
-            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Snacks</button>
-        </div>
+    <div className="ml-6">
+      <h3 className="text-xl font-semibold">Find the best food</h3>
+      <div className="my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-x-hidden">
+        {/* <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
+          All
+        </button> */}
+        <button className={`px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white ${selectedCategory === "All" && "bg-green-500 text-white"}`}  onClick={() => dispatch(setCategory("All"))}>
+            All
+          </button>
+        {categories.map((category, index) => (
+          <button className={`px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white ${selectedCategory === category && "bg-green-500 text-white"}`} key={index} onClick={() => dispatch(setCategory(category))}>
+            {category}
+          </button>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default CategoryMenu
+export default CategoryMenu;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CartSlice from "./slices/CartSlice.jsx"
+import CategorySlice from "./slices/CategorySlice.jsx"
 
 const store = configureStore({
     reducer: {
-        cart: CartSlice
+        cart: CartSlice,
+        category: CategorySlice
     }
 })
 export default store
